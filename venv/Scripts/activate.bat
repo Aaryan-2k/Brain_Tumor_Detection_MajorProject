8265c36e94ca2b42e2a97eab6185b03d9ac9a12f
Aaryan-2k <aaryantyagi17@gmail.com>
2024-05-13 00:04:22 +0530

    req update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Aaryan\Desktop\Major_Project\venv
+set VIRTUAL_ENV=C:\Users\Aaryan\Desktop\Major_Project\Brain_Tumor_Detection_MajorProject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
